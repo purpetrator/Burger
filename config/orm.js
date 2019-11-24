5216a7f41208cfcb55e9545d537c488f8a7bcfb2
Ana Chernov <anastasia.chernov@gmail.com>
2019-11-23 19:16:04 -0500

    Controller setup, created routers

--- config/orm.js
+++ config/orm.js
@@ -2,38 +2,38 @@
 var connection = require("../config/connection.js");
 
 // Helper functions for SQL syntax.
-// function printQuestionMarks(num) {
-//     var arr = [];
+function printQuestionMarks(num) {
+  var arr = [];
 
-//     for (var i = 0; i < num; i++) {
-//       arr.push("?");
-//     }
+  for (var i = 0; i < num; i++) {
+    arr.push("?");
+  }
 
-//     return arr.toString();
-//   }
+  return arr.toString();
+}
 
 // Helper function to convert object key/value pairs to SQL syntax
-// function objToSql(ob) {
-//   var arr = [];
+function objToSql(ob) {
+  var arr = [];
 
-//   // loop through the keys and push the key/value as a string int arr
-//   for (var key in ob) {
-//     var value = ob[key];
-//     // check to skip hidden properties
-//     if (Object.hasOwnProperty.call(ob, key)) {
-//       // if string with spaces, add quotations (Lana Del Grey => 'Lana Del Grey')
-//       if (typeof value === "string" && value.indexOf(" ") >= 0) {
-//         value = "'" + value + "'";
-//       }
-//       // e.g. {name: 'Lana Del Grey'} => ["name='Lana Del Grey'"]
-//       // e.g. {sleepy: true} => ["sleepy=true"]
-//       arr.push(key + "=" + value);
-//     }
-//   }
+  // loop through the keys and push the key/value as a string int arr
+  for (var key in ob) {
+    var value = ob[key];
+    // check to skip hidden properties
+    if (Object.hasOwnProperty.call(ob, key)) {
+      // if string with spaces, add quotations (Lana Del Grey => 'Lana Del Grey')
+      if (typeof value === "string" && value.indexOf(" ") >= 0) {
+        value = "'" + value + "'";
+      }
+      // e.g. {name: 'Lana Del Grey'} => ["name='Lana Del Grey'"]
+      // e.g. {sleepy: true} => ["sleepy=true"]
+      arr.push(key + "=" + value);
+    }
+  }
 
-//   // translate array of strings to a single comma-separated string
-//   return arr.toString();
-// }
+  // translate array of strings to a single comma-separated string
+  return arr.toString();
+}
 
 var orm = {
   selectAll: function(tableInput, cb) {
--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -5,5 +5,61 @@ var router = express.Router();
 // Import the model (burger.js) to use its database functions.
 var burger = require("../models/burger.js");
 
+router.get("/", function(req, res) {
+  burger.selectAll(function(data) {
+    var hbsObject = {
+      burgers: data
+    };
+    console.log(hbsObject);
+    res.render("index", hbsObject);
+  });
+});
+
+router.post("/api/burgers", function(req, res) {
+  burger.insertOne(
+    ["burger_name", "devoured"],
+    [req.body.burger_name, req.body.devoured],
+    function(result) {
+      // Send back the ID of the new quote
+      res.json({ id: result.insertId });
+    }
+  );
+});
+
+router.put("/api/burgers/:id", function(req, res) {
+  var burgerID = "id = " + req.params.id;
+
+  console.log("burgerID", burgerID);
+
+  burger.updateOne(
+    {
+      devoured: req.body.devoured
+    },
+    burgerID,
+    function(result) {
+      if (result.changedRows == 0) {
+        // If no rows were changed, then the ID must not exist, so 404
+        return res.status(404).end();
+      } else {
+        res.status(200).end();
+      }
+    }
+  );
+});
+
+router.delete("/api/burgers/:id", function(req, res) {
+  var burgerID = "id = " + req.params.id;
+  console.log("burgerID is " + burgerID);
+
+  burger.deleteOne(burgerID, function(result) {
+    if (result.affectedRows == 0) {
+      // If no rows were changed, then the ID must not exist, so 404
+      return res.status(404).end();
+    } else {
+      res.status(200).end();
+    }
+  });
+});
+
 // Export routes for server.js to use.
 module.exports = router;
