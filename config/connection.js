41773f65b9f2377867caceb0405215d195b912cc
Ana Chernov <anastasia.chernov@gmail.com>
2019-11-24 21:12:19 -0500

    Creating dotenv

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,17 @@
+require("dotenv").config();
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "lobster420",
-  database: "burgers_db"
-});
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: process.env.SQL_PASS,
+    database: "burgers_db"
+  });
+}
 
 // Make connection.
 connection.connect(function(err) {
